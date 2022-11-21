7b9d71bf781e54267d2f94305f45284d0f8ff07b
Jenya Korolev <korolev.evgen163@gmail.com>
2022-11-21 10:42:40 +0200

    express

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-const fs = require('fs');
+const express = require('express');
+require('dotenv').config();
+
+const userRouter = require('./router/user.router');
+const configs = require('./config/config');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/users',userRouter);
+
+app.get('/', (req, res) => {
+    res.json('WElCOME');
+});
+
+app.use((err, req, res, next) => {
+    res.status(err.status || 500).json({
+        message: err.message || 'Unknown error',
+        status: err.status || 500,
+    });
+});
+
+app.listen(configs.PORT, () => {
+    console.log(`Server listen ${configs.PORT}`);
+});
+
