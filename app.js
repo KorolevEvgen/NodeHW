ed2be5fcc6209ff28f9886f23a1874196e9736a0
Jenya Korolev <korolev.evgen163@gmail.com>
2022-11-23 09:24:18 +0200

    mongoose

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-const fs = require('fs');
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const userRouter = require('./router/user.router');
+const configs = require('./config/config');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/users', userRouter);
+
+app.get('/', (req, res) => {
+    res.json('WElCOME');
+});
+
+app.use((err, req, res, next) => {
+    res.status(err.status || 500).json({
+        message: err.message || 'Unknown error',
+        status: err.status || 500,
+    });
+});
+
+app.listen(configs.PORT,  async () => {
+    await mongoose.connect(configs.MONGO_URL)
+    console.log(`Server listen ${configs.PORT}`)
+});
+
