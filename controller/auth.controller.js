ac3bec7b3581199746c90a03044c8cd858c194a6
Jenya Korolev <korolev.evgen163@gmail.com>
2022-11-29 13:33:50 +0200

    Auth + joi

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -1,5 +1,5 @@
-const oauthService = require("../service/oauth.service");
-const OAuth = require("../dataBase/OAuth");
+const oauthService = require('../service/oauth.service');
+const OAuth = require('../dataBase/OAuth');
 
 module.exports = {
     login: async (req, res, next) => {
@@ -10,14 +10,29 @@ module.exports = {
 
             const tokenPair = oauthService.generateAccessTokenPair({ id: user._id });
 
-            await OAuth.create({ ...tokenPair, _user_id: user._id })
+            await OAuth.create({ ...tokenPair, _user_id: user._id });
 
             res.json({
                 user,
-                ...tokenPair
+                ...tokenPair,
             });
         } catch (e) {
             next(e);
         }
-    }
-}
+    },
+    refresh: async (req, res, next) => {
+        try {
+            const { refreshToken,_user_id } = req.tokenInfo;
+
+            await OAuth.deleteOne({ refreshToken });
+
+            const tokenPair = oauthService.generateAccessTokenPair({ id: _user_id });
+
+            await OAuth.create({ ...tokenPair, _user_id });
+
+            res.status(201).json(tokenPair);
+        } catch (e) {
+            next(e);
+        }
+    },
+};
