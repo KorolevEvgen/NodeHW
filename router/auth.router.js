627bb571b6472355410234468663145849709ce7
Jenya Korolev <korolev.evgen163@gmail.com>
2022-12-04 18:25:00 +0200

    Auth + joi

---
+++ router/auth.router.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+
+const controller = require('../controller/auth.controller');
+const mdlwr = require('../middleware/auth.middleware');
+const userMdlwr = require('../middleware/user.middleware');
+
+
+router.post('/login', mdlwr.isBodyValid, userMdlwr.getUserDynamically('email'), controller.login);
+
+router.post('/refresh',mdlwr.checkRefreshToken,controller.refresh);
+
+module.exports = router;
